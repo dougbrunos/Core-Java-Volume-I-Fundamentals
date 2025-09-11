59078181a9ab06175fa7a55ac66093a89ace0074
Douglas Bruno <dougbrunos@gmail.com>
2025-09-11 16:49:25 -0300

    Introduction to Object-Oriented Programming to Static Fields and Methods

---
+++ Chapter 4 - Objects and Classes/ObjectsAndObjectVariables.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class ObjectsAndObjectVariables {
+
+    public static void main(String[] args) {
+        System.out.println(new Date().toString());
+
+        Date startTime; // startTime doesn't refer to any object
+
+        Date endTime = new Date();
+
+        // Uses LocalDate to express days in the familiar calendar
+        System.out.println(LocalDate.now());
+        LocalDate newYearsEve = LocalDate.of(1999, 12, 31);
+        System.out.println(newYearsEve);
+        System.out.println(newYearsEve.getYear());
+        System.out.println(newYearsEve.getMonthValue());
+        System.out.println(newYearsEve.getDayOfMonth());
+
+        LocalDate aThousandDaysLater = newYearsEve.plusDays(1000);
+        System.out.println(aThousandDaysLater);
+    }
+
+}
---
+++ Chapter 4 - Objects and Classes/employee/Employee.java
@@ -0,0 +1,46 @@
+package employee;
+
+import java.time.LocalDate;
+
+public class Employee {
+
+    private String name;
+    private double salary;
+    private LocalDate hireDay;
+
+    public Employee(String n, double s, int year, int month, int day) {
+        name = n;
+        salary = s;
+        hireDay = LocalDate.of(year, month, day);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public LocalDate getHireDay() {
+        return hireDay;
+    }
+
+    public void setHireDay(LocalDate hireDay) {
+        this.hireDay = hireDay;
+    }
+
+    public void raiseSalary(double byPercent) {
+        double raise = this.salary * byPercent / 100;
+        this.salary += raise;
+    }
+
+}
